4f7ae276d7c418a176008f96a1782abe4c88204e
Preveen Raj <preveenraj.dev@gmail.com>
2024-08-31 13:59:57 +0530

    code refactoring after code analysis

--- app/src/main/java/com/techrush_app/goldrate/Container.kt
+++ app/src/main/java/com/techrush_app/goldrate/Container.kt
@@ -1,17 +1,13 @@
 package com.techrush_app.goldrate
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,33 +15,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalInspectionMode
-import androidx.compose.ui.text.PlatformTextStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.techrush_app.goldrate.ui.theme.GradientBackground
-import com.techrush_app.goldrate.ui.theme.LightBlue
-import com.techrush_app.goldrate.ui.theme.OffWhite
-import com.techrush_app.goldrate.ui.theme.Yellow
-import it.skrape.core.htmlDocument
-import it.skrape.fetcher.HttpFetcher
-import it.skrape.fetcher.response
-import it.skrape.fetcher.skrape
-import it.skrape.selects.html5.table
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Date
-import java.util.Locale
 
 data class Result(val rateString: String, val dateString: String, val dayStatus: String)
 
 @Composable
 @Preview
-public fun Container() {
+fun Container() {
     val backgrundGradient = GradientBackground.let {
         Brush.verticalGradient(
             colors = it,
@@ -74,9 +53,8 @@ public fun Container() {
             .fillMaxSize()
         ) {
             val isPreview = LocalInspectionMode.current
-            var data: Result;
 
-            data = when (isPreview) {
+            val data: Result = when (isPreview) {
                 true -> {
                     Result("5656", "31-Jan-24", "Today")
                 }
