a6f0b064ade6e87289ee29eeddef0a29b69ec2df
Preveen Raj <preveenraj.dev@gmail.com>
2024-09-05 17:58:38 +0530

    version 3.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.techrush_app.goldrate"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "3.1.1"
+        versionCode = 5
+        versionName = "3.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
