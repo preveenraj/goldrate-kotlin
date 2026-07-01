e0a0c91bf5c5798a8aecaf707669de7bbb52a5e6
Preveen Raj <preveenraj.dev@gmail.com>
2026-07-02 01:06:14 +0530

    release: 3.4.0 (pull-to-refresh, explore charts, daily notification)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.techrush_app.goldrate"
         minSdk = 24
         targetSdk = 35
-        versionCode = 13
-        versionName = "3.3.0"
+        versionCode = 14
+        versionName = "3.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
